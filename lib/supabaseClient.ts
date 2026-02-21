dceadac02dfc7813e64af1ffa8b72a73912b4c9a
DAEZKT <daezkt.training.wsyt@gmail.com>
2026-02-20 21:23:46 -0600

    Fix supabase credentials using env variables

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'AquiVaLaURLDelProyecto';
-const supabaseAnonKey = 'AcaVaLAanonKeyDelProyecto';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || '';
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY || '';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
